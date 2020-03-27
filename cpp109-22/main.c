03d7237b766f56335548152b35801ba6bc4e3886
Automaton Rob <dev@automatonrob.de>
2020-03-27 21:24:13 +0100

    [CPP109] 22 - minor changes

--- cpp109-22/main.c
+++ cpp109-22/main.c
@@ -1,19 +1,42 @@
 /*
- * Program receives the input of two integers and outputs a range of chars between the start and end value.
+ * Program receives the input of two integers and outputs the char codes and characters in between begin and end value.
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int begin, end, temp;
 
-    printf("Bitte erste Grenze eingeben (ganze Zahl): ");
-    fflush(stdin);
-    scanf("%i", &begin);
+    printf("##################################################");
+    printf("\nThe program outputs the char codes and characters.");
+    printf("\nOnly positive integers from 0 to 126 are allowed.");
+    printf("\n##################################################");
+    printf("\n\n");
 
-    printf("Bitte zweite Grenze eingeben (ganze Zahl): ");
+    printf("Please set char code to begin with: ");
     fflush(stdin);
-    scanf("%i", &end);
+    if (scanf("%i", &begin) != 1) {
+        printf("Invalid input!\n");
+        exit(1);
+    }
+
+    printf("Please set char code to end at: ");
+    fflush(stdin);
+    if (scanf("%i", &end) != 1) {
+        printf("Invalid input!\n");
+        exit(1);
+    }
+
+    if (begin < 0 || end < 0 ) {
+        printf("The entered range includes negative values that are not allowed!");
+        exit(1);
+    }
+
+    if (begin > 126 || end > 126) {
+        printf("An entered value exceeds the maximum of 126!");
+        exit(1);
+    }
 
     if (begin > end) {
         temp = end;
