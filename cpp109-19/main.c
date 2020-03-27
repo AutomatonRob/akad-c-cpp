015fcce4d008e9fee0ab5422134d377dac5cbb08
Automaton Rob <dev@automatonrob.de>
2020-03-27 21:24:02 +0100

    [CPP109] 19 - minor changes

--- cpp109-19/main.c
+++ cpp109-19/main.c
@@ -1,28 +1,53 @@
+/*
+ * Program reads two positive integers and returns the values of the different basic arithmetic operations
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes
+void calc();
 
 int main() {
-    int zahl1, zahl2;
-    int weiter = 1;
-
-    while (weiter == 1) {
-
-        printf("Zahl 1: ");
-        fflush(stdin);
-        scanf("%i", &zahl1);
-
-        printf("Zahl 2: ");
-        fflush(stdin);
-        scanf("%i", &zahl2);
-
-        printf("\n");
-        printf("%i + %i = %i\n", zahl1, zahl2, zahl1 + zahl2);
-        printf("%i - %i = %i\n", zahl1, zahl2, zahl1 - zahl2);
-        printf("%i * %i = %i\n", zahl1, zahl2, zahl1 * zahl2);
-        printf("%i / %i = %i\n", zahl1, zahl2, zahl1 / zahl2);
-
-        printf("Weiter (j/n)? ");
-        fflush(stdin);
-        scanf("%i", &weiter);
-    }
+    calc();
     return 0;
 }
+
+void calc() {
+    int int1 = 0;
+    int int2 = 0;
+    char proceed;
+
+    do {
+        proceed = ' ';
+
+        printf("Integer #1: ");
+        fflush(stdin);
+        scanf("%i", &int1);
+
+        printf("Integer #2 (!= 0): ");
+        fflush(stdin);
+        scanf("%i", &int2);
+
+        if (int2 == 0) {
+            printf("Division by 0 not allowed!");
+            exit(1);
+        }
+
+        printf("\n");
+        printf("%i + %i = %i\n", int1, int2, int1 + int2);
+        printf("%i - %i = %i\n", int1, int2, int1 - int2);
+        printf("%i * %i = %i\n", int1, int2, int1 * int2);
+        printf("%i / %i = %i\n", int1, int2, int1 / int2);
+        printf("\n");
+
+        while (proceed != 'y' && proceed != 'n') {
+            printf("Proceed (y/n)? ");
+            fflush(stdin);
+            scanf("%s", &proceed);
+        }
+    } while (proceed == 'y');
+    printf("\n\n\n");
+}
+
+
