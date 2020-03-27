13d44f83285144c550b331f40d7e9840a252bdf2
Automaton Rob <dev@automatonrob.de>
2020-03-27 21:45:09 +0100

    [CPP109] 23 - minor changes

--- cpp109-23/main.c
+++ cpp109-23/main.c
@@ -3,19 +3,40 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes
+void sum();
 
 int main() {
+    sum();
+    return 0;
+}
 
+void sum() {
     int start, end, temp, sum;
     sum = 0;
 
-    printf("Startwert: ");
-    fflush(stdin);
-    scanf("%i", &start);
+    printf("####################################################");
+    printf("\nSum up all integers within a certain range.");
+    printf("\nPositive and negative integers allowed, including 0.");
+    printf("\nEnter start and end of the desired range.");
+    printf("\n####################################################");
+    printf("\n\n");
 
-    printf("Endwert: ");
+    printf("Starting from integer: ");
     fflush(stdin);
-    scanf("%i", &end);
+    if(scanf("%d", &start) != 1) {
+        printf("Invalid input!");
+        exit(1);
+    }
+
+    printf("Ending at integer: ");
+    fflush(stdin);
+    if(scanf("%d", &end) != 1) {
+        printf("Invalid input!");
+        exit(1);
+    }
 
     if (start > end) {
         temp = end;
@@ -26,8 +47,5 @@ int main() {
     for (int i = start; i <= end; i++) {
         sum += i;
     }
-
-    printf("Summe: %i", sum);
-
-    return 0;
+    printf("Sum: %d", sum);
 }
